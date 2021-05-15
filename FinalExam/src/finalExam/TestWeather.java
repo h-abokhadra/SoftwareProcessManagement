0568e4ca556a9fd557bc40d28dba53dec6fbc2fb
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-05-15 19:01:33 -0400

    Uploading Project

---
+++ FinalExam/src/finalExam/TestWeather.java
@@ -0,0 +1,27 @@
+package finalExam;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class TestWeatherForecaster {
+
+	// 
+	@Test
+	void testgetWeatherForecastGood() {
+		double result = WeatherForecaster.getWeatherForecast(25, 1);
+		assertTrue("The quarters were returned correctly " + result, result == 27);
+	}
+	//
+
+	@Test
+	void getWeatherForecastBad(){
+		double result = WeatherForecaster.getWeatherForecast(25); // 
+		assertFalse("The quarters were returned incorrectly " + result, result == 30);
+	}
+
+}
